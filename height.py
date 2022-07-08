94abb4c00b1153847dd451b2bc995bfe92724405
winnergetsyou <97407880+winnergetsyou@users.noreply.github.com>
2022-07-08 01:25:17 -0400

    Update height.py
    CHANGED RANGE VARIABLE TO R AS PER CODE GRIPPER SUGGESTION

--- height.py
+++ height.py
@@ -2,9 +2,9 @@ class Height:
     def height(x,y,z):
        M= (x + y + z) / 3 # Mean M # Program can be used when we have more sensor readings
        calibration = 1000
-       Range = 65535
-       #print(M)
-       M= ( M * calibration ) / Range # Height in meter
+       R = 65535
+       #print(M)# R- RANGE
+       M= ( M * calibration ) / R # Height in meter
        print("height_in_meters")
        print(M)
        print("height_in_centi_meters")
